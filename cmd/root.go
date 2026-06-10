819045f4e6e8972ca394967e1818a9d31fe87663
Nicolas Legros <nlegros@coveo.com>
2026-06-10 12:26:14 -0400

    feat: add release pipeline with auto, GoReleaser, and PR title linting

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,21 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
-func NewRoot() *cobra.Command {
+func NewRoot(version string) *cobra.Command {
 	root := &cobra.Command{
 		Use:   "skl",
 		Short: "Manage agent skill installations from GitHub",
 	}
 	root.AddCommand(newInstall(), newUpdate(), newRemove(), newList())
+	root.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Print the version",
+		Run:   func(cmd *cobra.Command, args []string) { fmt.Println(version) },
+	})
 	return root
 }
